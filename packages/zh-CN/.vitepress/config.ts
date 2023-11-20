ed02ecabcab137db1627e121227ba9c8e4df3fe8
alex8088 <244096523@qq.com>
2023-11-20 22:57:26 +0800

    chore(zh): perfact algolia search translation

--- packages/zh-CN/.vitepress/config.ts
+++ packages/zh-CN/.vitepress/config.ts
@@ -72,8 +72,8 @@ export default defineConfig({
   lastUpdated: true,
   cleanUrls: true,
   locales: {
-    root: {label: '简体中文',},
-    en: {label: 'English',link: 'https://electron-vite.org/'}
+    root: { label: '简体中文' },
+    en: { label: 'English', link: 'https://electron-vite.org/' }
   },
   themeConfig: {
     logo: '/favicon.svg',
@@ -87,46 +87,49 @@ export default defineConfig({
       prev: '上一篇',
       next: '下一篇'
     },
-    algolia: {
-      appId: 'MP97N8JN52',
-      apiKey: 'fdeec9a7ea376e3133d94ccc832bd5aa',
-      indexName: 'cn-evite',
-      placeholder: '搜索文档',
-      translations: {
-        // button: {
-        //   buttonText: '搜索',
-        //   buttonAriaLabel: '搜索'
-        // },
-        modal: {
-          searchBox: {
-            resetButtonTitle: '清除查询条件',
-            resetButtonAriaLabel: '清除查询条件',
-            cancelButtonText: '取消',
-            cancelButtonAriaLabel: '取消'
+    search: {
+      provider: 'algolia',
+      options: {
+        appId: 'MP97N8JN52',
+        apiKey: 'fdeec9a7ea376e3133d94ccc832bd5aa',
+        indexName: 'cn-evite',
+        placeholder: '搜索文档',
+        translations: {
+          button: {
+            buttonText: '搜索',
+            buttonAriaLabel: '搜索'
           },
-          startScreen: {
-            recentSearchesTitle: '搜索历史',
-            noRecentSearchesText: '没有搜索历史',
-            saveRecentSearchButtonTitle: '保存到搜索历史',
-            removeRecentSearchButtonTitle: '从搜索历史中移除',
-            favoriteSearchesTitle: '收藏',
-            removeFavoriteSearchButtonTitle: '从收藏中移除'
-          },
-          errorScreen: {
-            titleText: '无法获取结果',
-            helpText: '你可能需要检查你的网络连接'
-          },
-          footer: {
-            selectText: '选择',
-            navigateText: '切换',
-            closeText: '关闭',
-            searchByText: '搜索供应商'
-          },
-          noResultsScreen: {
-            noResultsText: '无法找到相关结果',
-            suggestedQueryText: '你可以尝试查询',
-            reportMissingResultsText: '你认为这个查询应该有结果？',
-            reportMissingResultsLinkText: '向我们反馈'
+          modal: {
+            searchBox: {
+              resetButtonTitle: '清除查询条件',
+              resetButtonAriaLabel: '清除查询条件',
+              cancelButtonText: '取消',
+              cancelButtonAriaLabel: '取消'
+            },
+            startScreen: {
+              recentSearchesTitle: '搜索历史',
+              noRecentSearchesText: '没有搜索历史',
+              saveRecentSearchButtonTitle: '保存到搜索历史',
+              removeRecentSearchButtonTitle: '从搜索历史中移除',
+              favoriteSearchesTitle: '收藏',
+              removeFavoriteSearchButtonTitle: '从收藏中移除'
+            },
+            errorScreen: {
+              titleText: '无法获取结果',
+              helpText: '你可能需要检查你的网络连接'
+            },
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭',
+              searchByText: '搜索供应商'
+            },
+            noResultsScreen: {
+              noResultsText: '无法找到相关结果',
+              suggestedQueryText: '你可以尝试查询',
+              reportMissingResultsText: '你认为这个查询应该有结果？',
+              reportMissingResultsLinkText: '向我们反馈'
+            }
           }
         }
       }
