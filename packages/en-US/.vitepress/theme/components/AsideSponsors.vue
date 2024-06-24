d27522cbead71a899165423401d99437ff42378c
alex8088 <244096523@qq.com>
2024-06-24 21:23:02 +0800

    chore(en): sponsors

---
+++ packages/en-US/.vitepress/theme/components/AsideSponsors.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="VPDocAsideSponsors">
+  <div class="VPSponsors vp-sponsor aside">
+    <section class="vp-sponsor-section">
+      <div class="VPSponsorsGrid vp-sponsor-grid mini" data-vp-grid="1">
+        <div class="vp-sponsor-grid-item">
+          <a class="vp-sponsor-grid-link" href="https://chensuiyi.me" target="_blank" rel="sponsored noopener">
+            <article class="vp-sponsor-grid-box">
+              <h4 class="visually-hidden">前端之虎陈随易</h4>
+              <img style="max-width:36px; border-radius: 2px;" src="/sponsor-1.jpg" alt="sponsor-1">
+              <span class="vp-sponsor-grid-text" style="margin-left: 8px;">前端之虎陈随易</span>
+            </article>
+          </a>
+        </div>
+      </div>
+    </section>
+    <section class="vp-sponsor-section">
+      <div class="VPSponsorsGrid vp-sponsor-grid xmini" data-vp-grid="2">
+        <div class="vp-sponsor-grid-item">
+          <a class="vp-sponsor-grid-link" href="https://github.com/alex8088/electron-uikit" target="_blank" rel="sponsored noopener">
+            <article class="vp-sponsor-grid-box">
+              <h4 class="visually-hidden">electron-uikit</h4>
+              <img class="vp-sponsor-grid-image" src="/electron-uikit.svg" alt="electron-uikit">
+            </article>
+          </a>
+        </div>
+        <div class="vp-sponsor-grid-item">
+          <a class="vp-sponsor-grid-link" href="https://github.com/alex8088/electron-conf" target="_blank" rel="sponsored noopener">
+            <article class="vp-sponsor-grid-box">
+              <h4 class="visually-hidden">electron-conf</h4>
+              <img class="vp-sponsor-grid-image" src="/electron-conf.svg" alt="electron-conf">
+            </article>
+          </a>
+        </div>
+        <div class="vp-sponsor-grid-item">
+          <a class="vp-sponsor-grid-link" href="https://github.com/alex8088/sqlite3-queries" target="_blank" rel="sponsored noopener">
+            <article class="vp-sponsor-grid-box">
+              <h4 class="visually-hidden">sqlite3-queries</h4>
+              <img class="vp-sponsor-grid-image" src="/sqlite3-queries.svg" alt="sqlite3-queries">
+            </article>
+          </a>
+        </div>
+        <div class="vp-sponsor-grid-item">
+          <a class="vp-sponsor-grid-link" href="/sponsor/" rel="sponsored noopener">
+            <article class="vp-sponsor-grid-box">
+              <span class="vp-sponsor-grid-text" style="color: var(--vp-c-gray-1);">Your Logo</span>
+            </article>
+          </a>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+</template>
+
+<style>
+.vp-sponsor-grid-text {
+  color: var(--vp-c-text-2);
+  font-size: 12px;
+}
+
+.dark .vp-sponsor-grid-text {
+  color: var(--vp-c-gray-1);
+}
+</style>
--- packages/en-US/.vitepress/theme/index.ts
+++ packages/en-US/.vitepress/theme/index.ts
@@ -1,4 +1,15 @@
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './styles/custom.css'
 
-export default DefaultTheme
+import AsideSponsors from './components/AsideSponsors.vue'
+
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'aside-ads-before': () => h(AsideSponsors),
+    })
+  }
+}
+
