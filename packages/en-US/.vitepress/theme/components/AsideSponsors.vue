e3370a640c7894012ba3e4df90fa1dfe0550f630
alex8088 <244096523@qq.com>
2024-07-09 21:30:29 +0800

    chore: add sponsor

--- packages/en-US/.vitepress/theme/components/AsideSponsors.vue
+++ packages/en-US/.vitepress/theme/components/AsideSponsors.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const random = Math.floor(Math.random() * 3 + 1)
+</script>
+
 <template>
 <div class="VPDocAsideSponsors">
   <div class="VPSponsors vp-sponsor aside">
@@ -15,8 +19,21 @@
       </div>
     </section>
     <section class="vp-sponsor-section">
-      <div class="VPSponsorsGrid vp-sponsor-grid xmini" data-vp-grid="2">
+      <div class="VPSponsorsGrid vp-sponsor-grid mini" data-vp-grid="1">
         <div class="vp-sponsor-grid-item">
+          <a class="vp-sponsor-grid-link" href="https://dev.boxai.cc/share/14" target="_blank" rel="sponsored noopener">
+            <article class="vp-sponsor-grid-box">
+              <h4 class="visually-hidden">midjourney 绘画接口服务商</h4>
+              <img style="max-width:33px; border-radius: 2px;" src="/sponsor-2.svg" alt="sponsor-1">
+              <span class="vp-sponsor-grid-text" style="margin-left: 10px; text-align: center; line-height: 18px;">Midjourney<br/>绘画接口服务商</span>
+            </article>
+          </a>
+        </div>
+      </div>
+    </section>
+    <section class="vp-sponsor-section">
+      <div class="VPSponsorsGrid vp-sponsor-grid xmini" data-vp-grid="2">
+        <div class="vp-sponsor-grid-item" v-if="random === 1">
           <a class="vp-sponsor-grid-link" href="https://github.com/alex8088/electron-uikit" target="_blank" rel="sponsored noopener">
             <article class="vp-sponsor-grid-box">
               <h4 class="visually-hidden">electron-uikit</h4>
@@ -24,7 +41,7 @@
             </article>
           </a>
         </div>
-        <div class="vp-sponsor-grid-item">
+        <div class="vp-sponsor-grid-item" v-if="random === 2">
           <a class="vp-sponsor-grid-link" href="https://github.com/alex8088/electron-conf" target="_blank" rel="sponsored noopener">
             <article class="vp-sponsor-grid-box">
               <h4 class="visually-hidden">electron-conf</h4>
@@ -32,7 +49,7 @@
             </article>
           </a>
         </div>
-        <div class="vp-sponsor-grid-item">
+        <div class="vp-sponsor-grid-item" v-if="random === 3">
           <a class="vp-sponsor-grid-link" href="https://github.com/alex8088/sqlite3-queries" target="_blank" rel="sponsored noopener">
             <article class="vp-sponsor-grid-box">
               <h4 class="visually-hidden">sqlite3-queries</h4>
