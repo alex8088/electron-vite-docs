fa000675295ae753df0f94a8958649611e55706e
alex8088 <244096523@qq.com>
2025-03-05 23:42:50 +0800

    chore: add sponsor

--- packages/en-US/.vitepress/theme/components/AsideSponsors.vue
+++ packages/en-US/.vitepress/theme/components/AsideSponsors.vue
@@ -8,6 +8,19 @@ const random = Math.floor(Math.random() * 3 + 1)
     <section class="vp-sponsor-section">
       <div class="VPSponsorsGrid vp-sponsor-grid mini" data-vp-grid="1">
         <div class="vp-sponsor-grid-item">
+          <a class="vp-sponsor-grid-link" href="https://1ms.run?utm_source=electron-vite" target="_blank" rel="sponsored noopener">
+            <article class="vp-sponsor-grid-box">
+              <h4 class="visually-hidden">毫秒镜像</h4>
+              <img style="max-width:33px; border-radius: 2px;" src="/sponsor-3.svg" alt="sponsor-">
+              <span class="vp-sponsor-grid-text" style="margin-left: 10px; text-align: center; line-height: 18px; min-width: 85px; font-size: 14px;">毫秒镜像<br/><span style="font-size: 10px;">木雷坞 · MLIEV</span></span>
+            </article>
+          </a>
+        </div>
+      </div>
+    </section>
+    <section class="vp-sponsor-section">
+      <div class="VPSponsorsGrid vp-sponsor-grid mini" data-vp-grid="1">
+        <div class="vp-sponsor-grid-item">
           <a class="vp-sponsor-grid-link" href="https://dev.boxai.cc/share/14" target="_blank" rel="sponsored noopener">
             <article class="vp-sponsor-grid-box">
               <h4 class="visually-hidden">midjourney 绘画接口服务商</h4>
